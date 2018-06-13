7886c1f9c99cc58c92ea437bfaa00d4786b0c125
Julien <nospam.github@gribouille.eu>
2018-06-13 16:21:40 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-bundle-rickroller
+### Rickroller (Bundle)
+
+This bundle provides redirections from URL that botnets usually try, and sends them to Rick Astley. 
+Inspired by Nick Craver @StackOverflow
+
+### Requirements
+
+Polyfony 2.2+
---
+++ Rickroller/Controllers/Rickroller.php
@@ -0,0 +1,18 @@
+<?php
+
+class RickrollerController extends Polyfony\Controller {
+
+	public function RickrollerAction() {
+
+		// log
+		Polyfony\Logger::warning('Rick Astley has been trigerred, add thid IP to your blacklist.');
+		
+		// redirect
+		Polyfony\Response::setRedirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ',0);
+		Polyfony\Response::render();
+
+	}
+
+}
+
+?>
