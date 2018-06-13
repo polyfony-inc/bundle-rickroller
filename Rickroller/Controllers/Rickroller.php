8b3ab63e0deee0890da24a474de236d2b40d32d9
Julien <AnnoyingTechnology@users.noreply.github.com>
2018-06-13 16:22:48 +0200

    Typo

--- Rickroller/Controllers/Rickroller.php
+++ Rickroller/Controllers/Rickroller.php
@@ -5,7 +5,7 @@ class RickrollerController extends Polyfony\Controller {
 	public function RickrollerAction() {
 
 		// log
-		Polyfony\Logger::warning('Rick Astley has been trigerred, add thid IP to your blacklist.');
+		Polyfony\Logger::warning('Rick Astley has been trigerred, add this IP to your blacklist.');
 		
 		// redirect
 		Polyfony\Response::setRedirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ',0);
